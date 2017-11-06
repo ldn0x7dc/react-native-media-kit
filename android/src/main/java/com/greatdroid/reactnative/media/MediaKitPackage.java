c18fc249dac544ac0a5e1dd703368e60a3fa1bb1
Ronak <ronakkothari301@gmail.com>
2017-11-06 13:26:50 +0530

    Fixing crash in react-native 0.48.4 (Not overriding createJSModule method)

--- android/src/main/java/com/greatdroid/reactnative/media/MediaKitPackage.java
+++ android/src/main/java/com/greatdroid/reactnative/media/MediaKitPackage.java
@@ -19,7 +19,7 @@ public class MediaKitPackage implements ReactPackage {
   }
 
 
-  @Override
+  // @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
